4197764dfb058983beee0ea77d3e6a4e81cb8b57
Muhd Fauzan <muhdfauzan9302@gmail.com>
2019-07-25 10:40:14 +0800

    - Add vibrate function

--- redicode/src/main/java/my/com/fauzan/redicode/Util.java
+++ redicode/src/main/java/my/com/fauzan/redicode/Util.java
@@ -1,7 +1,11 @@
 package my.com.fauzan.redicode;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 
 public class Util {
 
@@ -9,4 +13,26 @@ public class Util {
         int result = context.checkCallingOrSelfPermission(permission);
         return result == PackageManager.PERMISSION_GRANTED;
     }
+
+    // Vibrate
+    @SuppressLint("MissingPermission")
+    public static void vibrate(Context context) {
+        try {
+            Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            // Vibrate for 500 milliseconds
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (v != null) {
+                    v.vibrate(VibrationEffect.createOneShot(300, VibrationEffect.DEFAULT_AMPLITUDE));
+                }
+            } else {
+                //deprecated in API 26
+                if (v != null) {
+                    v.vibrate(300);
+                }
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
 }
